bc13c8e0f406e4cec97f57f4d0ee89aae4fe7427
Ekaterina Kalashnikova <ladykati73@gmail.com>
2024-04-02 21:49:20 +0300

    7. Балансир [#505083]

--- src/main/java/ru/job4j/iterator/Balancer.java
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -8,37 +8,74 @@ import java.util.NoSuchElementException;
 public class Balancer {
     public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
         ArrayList<Integer> list = new ArrayList<>();
+        nodes.iterator();
         while (source.hasNext()) {
-            for (int j = 0; j < nodes.size(); j++) {
-                for (int k = 0; k < list.get(j); k++) {
-                      Integer i = source.next();
-                }
-                source = new Iterator<>() {
-                    int index = 0;
-
-                    @Override
-                    public boolean hasNext() {
-                        int count = index;
-                        while (list.get(index) == null) {
-                            index++;
-                            if (index == count || index == list.size()) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    }
-
-                    @Override
-                    public Integer next() {
-                        if (!hasNext()) {
-                            throw new NoSuchElementException();
-                        }
-                        return list.get(index++);
-                    }
-                };
-            }
+            list.add(source.next());
         }
+        new Iterator<>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                int count = index;
+                while (list.get(index) == null) {
+                    index++;
+                    if (index == list.size()) {
+                        index = 0;
+                    }
+                    if (index == count) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            @Override
+            public Object next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (index == list.size()) {
+                    index = 0;
+                }
+                return list.get(index++);
+            }
+        };
     }
 }
 
 
+/**
+ * "как правильно прочитать коллекцию source" - это не коллекция в строгом понимании, это итератор.
+ * Извлекаете из него элемент методом next(), а предварительно проверяете, что он есть методом hasNext().
+ * Так вот, если в итераторе есть элемент к выдаче, то записываете его  в очередной список из nodes,
+ * и так повторяете, пока итератор не ответит, что в нем закончились элементы к выдаче.
+ * while (source.hasNext()) {
+ * for (int j = 0; j < nodes.size(); j++) {
+ * for (int k = 0; k < list.get(j); k++) {
+ * Integer i = source.next();
+ * }
+ * <p>
+ * ArrayList<Integer> list = new ArrayList<>();
+ * <p>
+ * source = new Iterator<>() {
+ * int index = 0;
+ *
+ * @Override public boolean hasNext() {
+ * int count = index;
+ * while (list.get(index) == null) {
+ * index++;
+ * if (index == count || index == list.size()) {
+ * return false;
+ * }
+ * }
+ * return true;
+ * }
+ * @Override public Integer next() {
+ * if (!hasNext()) {
+ * throw new NoSuchElementException();
+ * }
+ * return list.get(index++);
+ * }
+ * };
+ */
